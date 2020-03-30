dfb493cf96e0b42c0f939c7734e37bffdde45a96
Oudwin <tristanmayofreire@gmail.com>
2020-03-30 21:03:59 +0200

    clip path not working

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.burger');
+const navUl = document.querySelector('.nav__ul');
+const navItems = document.querySelectorAll('.nav__item');
+
+hamburger.addEventListener('click', () => {
+  navUl.classList.add('.nav__ul--open');
+  console.log('toggled');
+});
